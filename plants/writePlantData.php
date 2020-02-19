5cf248fb71a5dcf79ded27849d801e2d90f6ca3a
Colin Sawyer <crsawyer45@gmail.com>
2020-02-19 21:28:18 +0000

    changin dir structure

---
+++ plants/writePlantData.php
@@ -0,0 +1,21 @@
+<?php
+
+    // Prepare variables for database connection
+    $dbusername = "arduino";  // enter database username
+    $dbpassword = "V4bGufj9SS35EetM!";  // enter database password
+    $server = "www.sawyerconcepts.com";
+    echo "hello world";
+    // Connect to database
+
+    $dbconnect = mysqli_connect($server, $dbusername, $dbpassword);
+    $dbselect = mysqli_select_db("Plants",$dbconnect);
+
+    // Prepare SQL statement
+
+    $sql = "INSERT INTO Plants.PlantInfo VALUES ('".$_GET["temp"]."', '".$_GET["humidity"]."', now())";
+
+    // Execute SQL statement
+
+    mysqli_query($dbconnect, $sql);
+
+?>
